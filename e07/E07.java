00912c2a6d0f9fa57d7e690a437470c211b56d95
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:50:06 +0300

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -4,13 +4,13 @@ public class E07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Which multiplicand multiplication table to show?");
+        System.out.println("Which multiplication table to show?");
         int multiplicand = scanner.nextInt();
 
-        System.out.println("Until which multiplier?");
+        System.out.println("Until which multiplier should the table show?");
         int multiplier = scanner.nextInt();
 
-        for (int i = 1; i <= multiplier; i++) {
+        for (int i = 0; i <= multiplier; i++) {
             int result = i * multiplicand;
             System.out.println(i + " x " + multiplicand + " = " + result);
         }
