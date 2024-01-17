e5dff4a2e68f6673bcb88d8da30a8f12abbf2921
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:35:34 +0300

    Update E09.java

--- e09/E09.java
+++ e09/E09.java
@@ -1,5 +1,29 @@
-class E09 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class E09 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+        int count = 0;
+
+        System.out.println("Give a test score (-1 to quit):");
+
+        while (true) {
+            int score = input.nextInt();
+
+            if (score == -1) {
+                break;
+            }
+
+            total += score;
+            count++;
+
+            double average = (double) total / count;
+            System.out.println("Average: " + average);
+            System.out.println("Give a test score (-1 to quit):");
+        }        
+
+        input.close();
     }
 }
