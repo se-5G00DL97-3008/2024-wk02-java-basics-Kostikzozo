0d5abb28ea46536f828ce7b9ba52c60c0d828793
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:11:06 +0300

    Update E05.java

--- e05/E05.java
+++ e05/E05.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class E05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -5,19 +7,34 @@ public class E05 {
         System.out.print("Enter a number between 1 and 7: ");
         int dayNumber = scanner.nextInt();
 
-       int dayNumber = scanner.nextInt();
-
-        // Validate the input range (1-7)
-        if (dayNumber >= 1 && dayNumber <= 7) {
-            
-            String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-            String day = daysOfWeek[dayNumber - 1];
-
-            // Print the result
-            System.out.println("The day corresponding to " + dayNumber + " is: " + day);
-        } else {
-            // Invalid input
-            System.out.println("Only 7 days in a week");
+        String day;
+        switch (dayNumber) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+            default:
+                day = "Tere are only 7 days in a week.";
+                break;
         }
+
+        System.out.println("day of the week: " + day);
     }
 }
