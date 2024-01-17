9a22480f09b0ebe525d3468dbf43ac05a3a32a4e
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 01:45:15 +0300

    Update E01.java

--- e01/E01.java
+++ e01/E01.java
@@ -1,5 +1,21 @@
-class E01 {
+public class E01 {
     public static void main(String[] args) {
+        int x = 10;
+        float y = 5.5f;
+        boolean flag = true;
+        char letter = 'c';
+        short year = 2022;
+        long population = 7000000000L;
+        byte num = 100;
+        double pi = 3.13457599923384753929348;
 
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("flag = " + flag);
+        System.out.println("letter = " + letter);
+        System.out.println("year = " + year);
+        System.out.println("population = " + population);
+        System.out.println("num = " + num);
+        System.out.println("pi = " + pi);
     }
 }
