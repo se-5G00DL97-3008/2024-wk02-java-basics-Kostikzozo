8053f153105387202f60a427f648c3e79c9d295d
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 01:54:02 +0300

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,19 @@
-class E03 {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class E03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("Number is positive");
+        } else if (number < 0) {
+            System.out.println("number is negative");
+        } else {
+            System.out.println("it is zero");
+        }
     }
 }
