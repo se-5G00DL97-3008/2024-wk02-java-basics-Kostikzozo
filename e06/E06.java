44f0f589d6909416de2089d660453e45f432c2fa
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:18:00 +0300

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,13 @@
-class E06 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class E03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("which multiplication table to show? ");
+        int number = input.nextInt();
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println((i + " x " + number + " = " + (i * number));
     }
 }
