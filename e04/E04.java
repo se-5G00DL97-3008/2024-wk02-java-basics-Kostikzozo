c65c6589264bb937560afc1013fe3c7b3a5b1b6f
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:52:27 +0300

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -7,7 +7,7 @@ class E04 {
         int day_n = input.nextInt();
         System.out.println("Enter a month: ");
         int month_n = input.nextInt();
-        if (day_n == 24) && (month_n == 12){
+        if ((day_n == 24) && (month_n == 12)){
             System.out.println("Merry Christmas!");
 
     }
