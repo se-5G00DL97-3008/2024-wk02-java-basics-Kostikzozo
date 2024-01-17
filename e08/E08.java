fb433d0dd90dfbd670fba4b60e91369092134981
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:30:44 +0300

    Update E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int number;
+
+        do {
+            System.out.println("Enter a number (0 to quit):");
+            number = scanner.nextInt();
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number % 2 == 0) {
+                System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        } while (true);
+
+        scanner.close();
     }
 }
