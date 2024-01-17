47b489dcb79b5b8eb773ec1ceba256e802512605
Kostikzozo <122258673+Kostikzozo@users.noreply.github.com>
2024-01-18 02:00:29 +0300

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 class E04 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a day: ");
+        int day_n = input.nextInt();
+        System.out.println("Enter a month: ");
+        int month_n = input.nextInt();
+        if (day_n == 24) && (month_n == 12){
+            System.out.println("Merry Christmas!");
 
     }
 }
